7f2d21003fba2c955279e14e7ce4b2680dbeb434
j-morag <moragj@post.bgu.ac.il>
2021-10-20 09:00:39 +0300

    added output to RunManagerMovingAIBenchmark

--- src/main/java/Environment/RunManagerMovingAIBenchmark.java
+++ src/main/java/Environment/RunManagerMovingAIBenchmark.java
@@ -1,27 +1,30 @@
 package Environment;
 
-import BasicCBS.Instances.InstanceBuilders.InstanceBuilder_BGU;
 import BasicCBS.Instances.InstanceBuilders.InstanceBuilder_MovingAI;
 import BasicCBS.Instances.InstanceManager;
 import BasicCBS.Instances.InstanceProperties;
-import BasicCBS.Instances.Maps.MapDimensions;
-import BasicCBS.Solvers.AStar.AllPairsShortestPathAStarHeuristic;
-import BasicCBS.Solvers.AStar.DistanceTableAStarHeuristic;
-import BasicCBS.Solvers.AStar.RunParameters_SAAStar;
-import BasicCBS.Solvers.AStar.SingleAgentAStar_Solver;
+import BasicCBS.Solvers.AStar.*;
 import BasicCBS.Solvers.PrioritisedPlanning.PrioritisedPlanning_Solver;
+import Environment.IO_Package.IO_Manager;
+import Environment.Metrics.InstanceReport;
+import Environment.Metrics.S_Metrics;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.stream.IntStream;
 
 public class RunManagerMovingAIBenchmark extends A_RunManager{
 
     private String entireBenchmarkDir = "";
     private Integer maxNumAgents = 60;
+    String resultsOutputDir = IO_Manager.buildPath(new String[]{System.getProperty("user.home"), "CBS_Results"});
 
     @Override
     void setSolvers() {
-//        super.solvers.add(new PrioritisedPlanning_Solver(new SingleAgentAStar_Solver(new RunParameters_SAAStar(new DistanceTableAStarHeuristic(new AllPairsShortestPathAStarHeuristic())))));
-        super.solvers.add(new PrioritisedPlanning_Solver(new SingleAgentAStar_Solver()));
+        super.solvers.add(new PrioritisedPlanning_Solver());
+        super.solvers.add(new PrioritisedPlanning_Solver(null, null, 4, null, PrioritisedPlanning_Solver.RestartStrategy.randomRestarts));
     }
 
     @Override
@@ -46,5 +49,56 @@ public class RunManagerMovingAIBenchmark extends A_RunManager{
         /*  =   Add new experiment   =  */
         Experiment EntireMovingAIBenchmark = new Experiment("EntireMovingAIBenchmark", instanceManager);
         this.experiments.add(EntireMovingAIBenchmark);
+    }
+
+    @Override
+    public void runAllExperiments() {
+        try {
+            S_Metrics.setHeader(new String[]{
+                    InstanceReport.StandardFields.experimentName,
+                    InstanceReport.StandardFields.instanceName,
+                    InstanceReport.StandardFields.numAgents,
+                    InstanceReport.StandardFields.solver,
+                    InstanceReport.StandardFields.solved,
+                    InstanceReport.StandardFields.valid,
+                    InstanceReport.StandardFields.elapsedTimeMS,
+                    InstanceReport.StandardFields.solutionCost,
+                    InstanceReport.StandardFields.solution});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        String pathWithStartTime = resultsOutputDir + "\\results " + dateFormat.format(System.currentTimeMillis()) + " .csv";
+        try {
+            S_Metrics.addOutputStream(new FileOutputStream((pathWithStartTime)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            S_Metrics.addOutputStream(System.out, S_Metrics::instanceReportToHumanReadableString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        super.runAllExperiments();
+        String pathWithEndTime = resultsOutputDir + "\\results " + dateFormat.format(System.currentTimeMillis()) + " .csv";
+
+        try {
+            S_Metrics.exportCSV(new FileOutputStream(pathWithEndTime)
+//                    ,new String[]{   InstanceReport.StandardFields.experimentName,
+//                            InstanceReport.StandardFields.experimentName,
+//                            InstanceReport.StandardFields.numAgents,
+//                            InstanceReport.StandardFields.solver,
+//                            InstanceReport.StandardFields.solved,
+//                            InstanceReport.StandardFields.valid,
+//                            InstanceReport.StandardFields.elapsedTimeMS,
+//                            InstanceReport.StandardFields.solutionCost,
+//                            InstanceReport.StandardFields.solution}
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        S_Metrics.clearAll();
     }
 }
