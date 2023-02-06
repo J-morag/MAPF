e806e4984f831d4b752dd7f24bfcb28f5fc49ccd
J-morag <38430800+J-morag@users.noreply.github.com>
2023-02-06 15:31:20 +0200

    Factor out astar goal check (#30)
    * factor goal condition out of AStar
    
    * Added support for PIBT-style goal condition (only in AStar!)

--- src/main/java/BasicMAPF/Solvers/AStar/RunParameters_SAAStar.java
+++ src/main/java/BasicMAPF/Solvers/AStar/RunParameters_SAAStar.java
@@ -2,6 +2,7 @@ package BasicMAPF.Solvers.AStar;
 
 import BasicMAPF.Instances.Maps.Coordinates.I_Coordinate;
 import BasicMAPF.Solvers.AStar.CostsAndHeuristics.AStarGAndH;
+import BasicMAPF.Solvers.AStar.GoalConditions.I_AStarGoalCondition;
 import BasicMAPF.Solvers.ConstraintsAndConflicts.ConflictManagement.I_ConflictAvoidanceTable;
 import Environment.Metrics.InstanceReport;
 import BasicMAPF.Solvers.ConstraintsAndConflicts.Constraint.ConstraintSet;
@@ -18,6 +19,11 @@ public class RunParameters_SAAStar extends RunParameters {
     public I_Coordinate sourceCoor;
     public I_Coordinate targetCoor;
     public float fBudget = Float.POSITIVE_INFINITY;
+    public I_AStarGoalCondition goalCondition;
+
+    public RunParameters_SAAStar(RunParameters parameters){
+        super(parameters);
+    }
 
     public RunParameters_SAAStar(long timeout, ConstraintSet constraints, InstanceReport instanceReport, Solution existingSolution, AStarGAndH heuristicFunction) {
         super(timeout, constraints, instanceReport, existingSolution);
