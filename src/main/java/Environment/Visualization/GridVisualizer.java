e65d8dbb3f77f2c111cbaf1bfbb8e0f47bda513a
J-morag <moragj@post.bgu.ac.il>
2023-02-15 12:32:32 +0200

    clean imports

--- src/main/java/Environment/Visualization/GridVisualizer.java
+++ src/main/java/Environment/Visualization/GridVisualizer.java
@@ -2,8 +2,6 @@ package Environment.Visualization;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
