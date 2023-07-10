948ef77a169edaf50d2a2e94abbc11962d76b27d
J-morag <moragj@post.bgu.ac.il>
2023-07-10 21:29:50 +0300

    corrected lookahead length

--- src/main/java/LifelongMAPF/LifelongRunManagers/A_LifelongRunManager.java
+++ src/main/java/LifelongMAPF/LifelongRunManagers/A_LifelongRunManager.java
@@ -164,22 +164,22 @@ public abstract class A_LifelongRunManager extends A_RunManager {
 
         LifelongSimulationSolver lifelong_PrPT_SOC = new LifelongSimulationSolver(null, new StationaryAgentsSubsetSelector(new PeriodicSelector(3)),
                 PrPT_SOC,
-                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 1);
+                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 3);
         lifelong_PrPT_SOC.name = "lifelong_PrPT_SOC";
 
         LifelongSimulationSolver lifelong_PrP_SOC = new LifelongSimulationSolver(null, new StationaryAgentsSubsetSelector(new PeriodicSelector(3)),
                 PrP_SOC,
-                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 1);
+                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 3);
         lifelong_PrP_SOC.name = "lifelong_PrP_SOC";
 
         LifelongSimulationSolver lifelong_PrPT_SST = new LifelongSimulationSolver(null, new StationaryAgentsSubsetSelector(new PeriodicSelector(3)),
                 PrPT_SST,
-                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 1);
+                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 3);
         lifelong_PrPT_SST.name = "lifelong_PrPT_SST";
 
         LifelongSimulationSolver lifelong_PrP_SST = new LifelongSimulationSolver(null, new StationaryAgentsSubsetSelector(new PeriodicSelector(3)),
                 PrP_SST,
-                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 1);
+                null, new DeepPartialSolutionsStrategy(), new OneActionFailPolicy(true), 3);
         lifelong_PrP_SST.name = "lifelong_PrP_SST";
 
         solvers.add(lifelong_PrPT_SOC);
