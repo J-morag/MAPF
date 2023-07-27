09ebcad83749ff59b37c822c860fb938ba119ba2
Nadav Arbel <94472346+arbelna@users.noreply.github.com>
2023-07-27 12:46:58 +0300

    Update A_Solver.java

--- src/main/java/BasicMAPF/Solvers/A_Solver.java
+++ src/main/java/BasicMAPF/Solvers/A_Solver.java
@@ -110,7 +110,6 @@ public abstract class A_Solver implements I_Solver{
         if(solution != null){
             instanceReport.putStringValue(InstanceReport.StandardFields.solution, solution.toString());
             instanceReport.putIntegerValue(InstanceReport.StandardFields.solved, 1);
-            instanceReport.putSolution(solution);
         }
         else{
             instanceReport.putIntegerValue(InstanceReport.StandardFields.solved, 0);
