ae272ba2a5c442a09ff754c3f4058226a08f3dd0
J-morag <38430800+J-morag@users.noreply.github.com>
2023-11-08 13:39:55 +1100

    Refactor priority order from RunParameters_PP into RunParameters

--- src/main/java/BasicMAPF/Solvers/AStar/RunParameters_SAAStar.java
+++ src/main/java/BasicMAPF/Solvers/AStar/RunParameters_SAAStar.java
@@ -1,13 +1,9 @@
 package BasicMAPF.Solvers.AStar;
 
 import BasicMAPF.Instances.Maps.Coordinates.I_Coordinate;
-import BasicMAPF.Solvers.AStar.CostsAndHeuristics.AStarGAndH;
 import BasicMAPF.Solvers.AStar.GoalConditions.I_AStarGoalCondition;
 import BasicMAPF.Solvers.ConstraintsAndConflicts.ConflictManagement.ConflictAvoidance.I_ConflictAvoidanceTable;
-import Environment.Metrics.InstanceReport;
-import BasicMAPF.Solvers.ConstraintsAndConflicts.Constraint.ConstraintSet;
 import BasicMAPF.DataTypesAndStructures.RunParameters;
-import BasicMAPF.DataTypesAndStructures.Solution;
 
 public class RunParameters_SAAStar extends RunParameters {
     public I_ConflictAvoidanceTable conflictAvoidanceTable;
