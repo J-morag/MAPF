cf25d1550f3abec2ce5d3ee7671314b848a44422
J-morag <38430800+J-morag@users.noreply.github.com>
2023-11-08 16:05:49 +1100

    Added ICTS stress test

--- src/test/java/BasicMAPF/Solvers/StressTests.java
+++ src/test/java/BasicMAPF/Solvers/StressTests.java
@@ -9,6 +9,7 @@ import BasicMAPF.Instances.InstanceProperties;
 import BasicMAPF.Instances.MAPF_Instance;
 import BasicMAPF.Solvers.AStar.SingleAgentAStar_Solver;
 import BasicMAPF.Solvers.CBS.CBS_Solver;
+import BasicMAPF.Solvers.ICTS.HighLevel.ICTS_Solver;
 import BasicMAPF.Solvers.LargeNeighborhoodSearch.LargeNeighborhoodSearch_Solver;
 import BasicMAPF.Solvers.PrioritisedPlanning.PrioritisedPlanning_Solver;
 import BasicMAPF.Solvers.PrioritisedPlanning.RestartsStrategy;
@@ -30,6 +31,14 @@ public class StressTests {
     @Test
     public void CBSStressTest() {
         I_Solver solver = new CBS_Solver();
+        long timeout = 1000 * 60;
+        int numAgents = 30;
+        StressTest(solver, timeout, numAgents);
+    }
+
+    @Test
+    public void ICTSStressTest() {
+        I_Solver solver = new ICTS_Solver();
         long timeout = 1000 * 60;
         int numAgents = 30;
         StressTest(solver, timeout, numAgents);
