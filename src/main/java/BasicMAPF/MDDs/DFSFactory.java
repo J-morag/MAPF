5b902785ecfe8fcbf9a308a5ebf36b05bbffa46f
J-morag <38430800+J-morag@users.noreply.github.com>
2023-11-13 15:43:36 +1100

    remove DistanceTableSingleAgentHeuristicMDD and just use SingleAgentGAndH

--- src/main/java/BasicMAPF/MDDs/DFSFactory.java
+++ src/main/java/BasicMAPF/MDDs/DFSFactory.java
@@ -3,6 +3,7 @@ package BasicMAPF.MDDs;
 import BasicMAPF.DataTypesAndStructures.Timeout;
 import BasicMAPF.Instances.Agent;
 import BasicMAPF.Instances.Maps.I_Location;
+import BasicMAPF.Solvers.AStar.CostsAndHeuristics.SingleAgentGAndH;
 
 public class DFSFactory implements I_MDDSearcherFactory {
 
@@ -14,7 +15,7 @@ public class DFSFactory implements I_MDDSearcherFactory {
     }
 
     @Override
-    public A_MDDSearcher createSearcher(Timeout timeout, I_Location source, I_Location target, Agent agent, DistanceTableSingleAgentHeuristicMDD heuristic) {
+    public A_MDDSearcher createSearcher(Timeout timeout, I_Location source, I_Location target, Agent agent, SingleAgentGAndH heuristic) {
         return new DFSMDDBuilder(timeout, source, target, agent, heuristic, disappearAtGoal);
     }
 
