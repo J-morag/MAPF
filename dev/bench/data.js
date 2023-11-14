89351c7bd8c09a0e7df6eae36120f326adf1d5c3
github-action-benchmark <github@users.noreply.github.com>
2023-11-14 05:44:48 +0000

    add AStar Benchmark (customSmallerIsBetter) benchmark result for acb6106bf9563a6efcb54ee039666df9cded0ec9

---
+++ dev/bench/data.js
@@ -0,0 +1,58 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1699940688825,
+  "repoUrl": "https://github.com/J-morag/MAPF",
+  "entries": {
+    "AStar Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "id": "acb6106bf9563a6efcb54ee039666df9cded0ec9",
+          "message": "performance benchmarking instead of stress test",
+          "timestamp": "2023-10-06T06:21:24Z",
+          "url": "https://github.com/J-morag/MAPF/pull/70/commits/acb6106bf9563a6efcb54ee039666df9cded0ec9"
+        },
+        "date": 1699940687761,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "AStar - Failed",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "AStar - Average Cost",
+            "value": 0,
+            "unit": "SOC"
+          },
+          {
+            "name": "AStar - Average Runtime",
+            "value": 3.8235295,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "AStar - Average Expansions Low Level",
+            "value": 1115.8235,
+            "unit": "Expansions"
+          }
+        ]
+      }
+    ]
+  }
+}
