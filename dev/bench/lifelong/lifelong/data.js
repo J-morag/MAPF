f82c767505a463998dd76af9d3fadeea189a4eaf
github-action-benchmark <github@users.noreply.github.com>
2023-11-15 04:02:59 +0000

    add stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 Benchmark (customSmallerIsBetter) benchmark result for 5ee4a19c938384ebb0695fa2bb3b18c4ee910ea1

---
+++ dev/bench/lifelong/lifelong/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1700020979379,
+  "repoUrl": "https://github.com/J-morag/MAPF",
+  "entries": {
+    "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "distinct": true,
+          "id": "5ee4a19c938384ebb0695fa2bb3b18c4ee910ea1",
+          "message": "remove irrelevant metrics from lifelong performance benchmark",
+          "timestamp": "2023-11-15T14:25:58+11:00",
+          "tree_id": "a4be43933543457f827095c966fb61a9a045e934",
+          "url": "https://github.com/J-morag/MAPF/commit/5ee4a19c938384ebb0695fa2bb3b18c4ee910ea1"
+        },
+        "date": 1700020978204,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 - Average Throughput",
+            "value": 461.66666,
+            "unit": "Throughput @ T=200"
+          },
+          {
+            "name": "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 - Average Runtime",
+            "value": 102011.164,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 - Average Runtime Low Level",
+            "value": 67636.664,
+            "unit": "Milliseconds"
+          }
+        ]
+      }
+    ]
+  }
+}
