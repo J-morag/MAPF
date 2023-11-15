21e98c48389fd589d1500926925534567b1d83f3
github-action-benchmark <github@users.noreply.github.com>
2023-11-15 03:53:49 +0000

    add PrP + randomRestarts x10 Benchmark (customSmallerIsBetter) benchmark result for 5ee4a19c938384ebb0695fa2bb3b18c4ee910ea1

--- dev/bench/lifelong/offline/data.js
+++ dev/bench/lifelong/offline/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700020428742,
+  "lastUpdate": 1700020429830,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -109,6 +109,62 @@ window.BENCHMARK_DATA = {
           {
             "name": "ICTS_Solver_S2P - Average Expansions Low Level",
             "value": 54494.832,
+            "unit": "Expansions"
+          }
+        ]
+      }
+    ],
+    "PrP + randomRestarts x10 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "distinct": true,
+          "id": "5ee4a19c938384ebb0695fa2bb3b18c4ee910ea1",
+          "message": "remove irrelevant metrics from lifelong performance benchmark",
+          "timestamp": "2023-11-15T14:25:58+11:00",
+          "tree_id": "a4be43933543457f827095c966fb61a9a045e934",
+          "url": "https://github.com/J-morag/MAPF/commit/5ee4a19c938384ebb0695fa2bb3b18c4ee910ea1"
+        },
+        "date": 1700020429545,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PrP + randomRestarts x10 - Fails",
+            "value": 2,
+            "unit": "Instances"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Cost",
+            "value": 4956.1333,
+            "unit": "SOC"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Runtime",
+            "value": 8979.667,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Runtime Low Level",
+            "value": 8043,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Expansions Low Level",
+            "value": 3815735,
             "unit": "Expansions"
           }
         ]
