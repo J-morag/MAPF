545b283acf1f363233f6021587cc3db1be9b3d5d
github-action-benchmark <github@users.noreply.github.com>
2023-11-15 01:32:40 +0000

    add PIBT Benchmark (customSmallerIsBetter) benchmark result for dcfa38d81f56de2cdd6d013ce801e78b9945ea0b

--- dev/bench/lifelong/offline/data.js
+++ dev/bench/lifelong/offline/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700011959462,
+  "lastUpdate": 1700011960502,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -221,6 +221,62 @@ window.BENCHMARK_DATA = {
           {
             "name": "ALNS - Average Expansions Low Level",
             "value": 10544480,
+            "unit": "Expansions"
+          }
+        ]
+      }
+    ],
+    "PIBT Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "distinct": true,
+          "id": "dcfa38d81f56de2cdd6d013ce801e78b9945ea0b",
+          "message": "continuous performance benchmarking for lifelong",
+          "timestamp": "2023-11-15T12:04:51+11:00",
+          "tree_id": "02e81e9dca42190614d77067c0327e1428db638a",
+          "url": "https://github.com/J-morag/MAPF/commit/dcfa38d81f56de2cdd6d013ce801e78b9945ea0b"
+        },
+        "date": 1700011960209,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PIBT - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "PIBT - Average Cost",
+            "value": 5034.647,
+            "unit": "SOC"
+          },
+          {
+            "name": "PIBT - Average Runtime",
+            "value": 120.35294,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PIBT - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PIBT - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "PIBT - Average Expansions Low Level",
+            "value": 0,
             "unit": "Expansions"
           }
         ]
