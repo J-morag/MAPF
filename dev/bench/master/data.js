3bbd4eaf3c35ee554f3b3d7f3a6c2ed11554b879
github-action-benchmark <github@users.noreply.github.com>
2023-11-19 13:45:55 +0000

    add AStar Benchmark (customSmallerIsBetter) benchmark result for 588361ebe6de92d988921e1aa26849a37fed33a2

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700401554816,
+  "lastUpdate": 1700401555900,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -912,6 +912,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "AStar - Average Runtime",
             "value": 5.4117646,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "AStar - Average Expansions Low Level",
+            "value": 1115.8235,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "104091867+NoyGabay@users.noreply.github.com",
+            "name": "NoyGabay",
+            "username": "NoyGabay"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "588361ebe6de92d988921e1aa26849a37fed33a2",
+          "message": "infinite constraints support for PIBT, with tests (#69)\n\n* infinite constraints support for PIBT, with tests\r\n\r\n* infinite constraints addition, conflicts resolved",
+          "timestamp": "2023-11-19T15:17:48+02:00",
+          "tree_id": "53e3640b8928063d4e2ae43e57a17d2c94157b72",
+          "url": "https://github.com/J-morag/MAPF/commit/588361ebe6de92d988921e1aa26849a37fed33a2"
+        },
+        "date": 1700401555640,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "AStar - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "AStar - Average Cost",
+            "value": 89.117645,
+            "unit": "SOC"
+          },
+          {
+            "name": "AStar - Average Runtime",
+            "value": 4.8235292,
             "unit": "Milliseconds"
           },
           {
