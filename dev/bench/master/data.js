69fe94fa471268017fcb2e3d2df010980b817dd7
github-action-benchmark <github@users.noreply.github.com>
2023-11-20 03:12:33 +0000

    add PrP + randomRestarts x10 Benchmark (customSmallerIsBetter) benchmark result for 45ffc525fa65f51d0045dd0e6174208e1cf2c4a7

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700449952394,
+  "lastUpdate": 1700449953407,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -597,6 +597,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "PrP + randomRestarts x10 - Average Expansions Low Level",
             "value": 3836960,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "45ffc525fa65f51d0045dd0e6174208e1cf2c4a7",
+          "message": "Increase performance regression alert threshold to 200%",
+          "timestamp": "2023-11-20T13:45:21+11:00",
+          "tree_id": "f47cfb07bc3193161dfaf139e04c50025bb01533",
+          "url": "https://github.com/J-morag/MAPF/commit/45ffc525fa65f51d0045dd0e6174208e1cf2c4a7"
+        },
+        "date": 1700449953113,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PrP + randomRestarts x10 - Fails",
+            "value": 2,
+            "unit": "Instances"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Cost",
+            "value": 4956.1333,
+            "unit": "SOC"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Runtime",
+            "value": 9239.4,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Runtime Low Level",
+            "value": 8097.067,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "PrP + randomRestarts x10 - Average Expansions Low Level",
+            "value": 3704498.2,
             "unit": "Expansions"
           }
         ]
