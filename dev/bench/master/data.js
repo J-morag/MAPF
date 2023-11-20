b3cf3cd2df45bf288135578ff699f6f3dfb05434
github-action-benchmark <github@users.noreply.github.com>
2023-11-20 03:12:30 +0000

    add CBS Benchmark (customSmallerIsBetter) benchmark result for 45ffc525fa65f51d0045dd0e6174208e1cf2c4a7

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700401555900,
+  "lastUpdate": 1700449950855,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -161,6 +161,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "CBS - Average Expansions Low Level",
             "value": 749600.44,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "45ffc525fa65f51d0045dd0e6174208e1cf2c4a7",
+          "message": "Increase performance regression alert threshold to 200%",
+          "timestamp": "2023-11-20T13:45:21+11:00",
+          "tree_id": "f47cfb07bc3193161dfaf139e04c50025bb01533",
+          "url": "https://github.com/J-morag/MAPF/commit/45ffc525fa65f51d0045dd0e6174208e1cf2c4a7"
+        },
+        "date": 1700449950228,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CBS - Fails",
+            "value": 9,
+            "unit": "Instances"
+          },
+          {
+            "name": "CBS - Average Cost",
+            "value": 3448.25,
+            "unit": "SOC"
+          },
+          {
+            "name": "CBS - Average Runtime",
+            "value": 3783.5,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Runtime Low Level",
+            "value": 2384.5,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Expansions High Level",
+            "value": 5224.75,
+            "unit": "Expansions"
+          },
+          {
+            "name": "CBS - Average Expansions Low Level",
+            "value": 1003313.5,
             "unit": "Expansions"
           }
         ]
