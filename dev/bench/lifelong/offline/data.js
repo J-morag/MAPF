7fc415324611767fb678acc6adc52647bf608aa8
github-action-benchmark <github@users.noreply.github.com>
2023-11-21 10:54:16 +0000

    add CBS Benchmark (customSmallerIsBetter) benchmark result for 74d4be56da9bfb4353fb15c72f79e9da0b45f6ea

--- dev/bench/lifelong/offline/data.js
+++ dev/bench/lifelong/offline/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700020433040,
+  "lastUpdate": 1700564056008,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -53,6 +53,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "CBS - Average Expansions Low Level",
             "value": 749826.56,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "74d4be56da9bfb4353fb15c72f79e9da0b45f6ea",
+          "message": "Deafult \"Infinite\" timeoutEach in Lifelong (#72)",
+          "timestamp": "2023-11-21T21:26:36+11:00",
+          "tree_id": "b045aa3470a52cd202af68d38b5f7f4d66686a5f",
+          "url": "https://github.com/J-morag/MAPF/commit/74d4be56da9bfb4353fb15c72f79e9da0b45f6ea"
+        },
+        "date": 1700564055336,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CBS - Fails",
+            "value": 9,
+            "unit": "Instances"
+          },
+          {
+            "name": "CBS - Average Cost",
+            "value": 3448.25,
+            "unit": "SOC"
+          },
+          {
+            "name": "CBS - Average Runtime",
+            "value": 4063.625,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Runtime Low Level",
+            "value": 2335.5,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Expansions High Level",
+            "value": 7139.375,
+            "unit": "Expansions"
+          },
+          {
+            "name": "CBS - Average Expansions Low Level",
+            "value": 1110750.6,
             "unit": "Expansions"
           }
         ]
