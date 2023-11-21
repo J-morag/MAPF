2428a8a99c110f5ccae1761729440e8e70fd48c8
github-action-benchmark <github@users.noreply.github.com>
2023-11-21 10:54:19 +0000

    add ALNS Benchmark (customSmallerIsBetter) benchmark result for 74d4be56da9bfb4353fb15c72f79e9da0b45f6ea

--- dev/bench/lifelong/offline/data.js
+++ dev/bench/lifelong/offline/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700564058785,
+  "lastUpdate": 1700564059882,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -383,6 +383,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ALNS - Average Expansions Low Level",
             "value": 10477071,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "74d4be56da9bfb4353fb15c72f79e9da0b45f6ea",
+          "message": "Deafult \"Infinite\" timeoutEach in Lifelong (#72)",
+          "timestamp": "2023-11-21T21:26:36+11:00",
+          "tree_id": "b045aa3470a52cd202af68d38b5f7f4d66686a5f",
+          "url": "https://github.com/J-morag/MAPF/commit/74d4be56da9bfb4353fb15c72f79e9da0b45f6ea"
+        },
+        "date": 1700564059600,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ALNS - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "ALNS - Average Cost",
+            "value": 4515.706,
+            "unit": "SOC"
+          },
+          {
+            "name": "ALNS - Average Runtime",
+            "value": 30003.412,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ALNS - Average Runtime Low Level",
+            "value": 28889.824,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ALNS - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ALNS - Average Expansions Low Level",
+            "value": 8455064,
             "unit": "Expansions"
           }
         ]
