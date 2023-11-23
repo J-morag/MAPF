39dfc1b992d596c32513daa3bd1cfe85208a42bd
github-action-benchmark <github@users.noreply.github.com>
2023-11-23 06:35:44 +0000

    add PIBT Benchmark (customSmallerIsBetter) benchmark result for 1c37526cff08cc27bc15b8c55fb32a8280d7b164

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700721343821,
+  "lastUpdate": 1700721344866,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -1666,6 +1666,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "PIBT - Average Runtime",
             "value": 568.58826,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PIBT - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PIBT - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "PIBT - Average Expansions Low Level",
+            "value": 0,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c37526cff08cc27bc15b8c55fb32a8280d7b164",
+          "message": "make NoStateTimeSearches thread-safe (#73)",
+          "timestamp": "2023-11-23T17:07:10+11:00",
+          "tree_id": "2f7d27bdc08f00a0716a9bd293e76914a3437bd4",
+          "url": "https://github.com/J-morag/MAPF/commit/1c37526cff08cc27bc15b8c55fb32a8280d7b164"
+        },
+        "date": 1700721344570,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PIBT - Fails",
+            "value": 6,
+            "unit": "Instances"
+          },
+          {
+            "name": "PIBT - Average Cost",
+            "value": 27649.727,
+            "unit": "SOC"
+          },
+          {
+            "name": "PIBT - Average Runtime",
+            "value": 576.17645,
             "unit": "Milliseconds"
           },
           {
