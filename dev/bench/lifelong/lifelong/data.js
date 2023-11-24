0ffa7576fbe3b95aedcb5e561583eeb722fdf59f
github-action-benchmark <github@users.noreply.github.com>
2023-11-24 10:38:17 +0000

    add stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 Benchmark (customBiggerIsBetter) benchmark result for 63b0ef58aef18d1a3c19645cc697e42288899a65

--- dev/bench/lifelong/lifelong/data.js
+++ dev/bench/lifelong/lifelong/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700815827923,
+  "lastUpdate": 1700822297513,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 Benchmark": [
@@ -272,6 +272,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 - Average Runtime Low Level  (Reciprocal)",
             "value": 0.000016410795,
+            "unit": "1 / Milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "distinct": true,
+          "id": "63b0ef58aef18d1a3c19645cc697e42288899a65",
+          "message": "Merge branch 'master' into lifelong",
+          "timestamp": "2023-11-24T21:07:24+11:00",
+          "tree_id": "a61d249d61dde8389dbe72a6dbc167c6975b242f",
+          "url": "https://github.com/J-morag/MAPF/commit/63b0ef58aef18d1a3c19645cc697e42288899a65"
+        },
+        "date": 1700822296299,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 - Average Throughput",
+            "value": 464.66666,
+            "unit": "Throughput @ T=200"
+          },
+          {
+            "name": "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 - Average Runtime (Reciprocal)",
+            "value": 0.000013652499,
+            "unit": "1 / Milliseconds"
+          },
+          {
+            "name": "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 - Average Runtime Low Level  (Reciprocal)",
+            "value": 0.000016418699,
             "unit": "1 / Milliseconds"
           }
         ]
