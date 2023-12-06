234a27f619fa687291e618501581b1986150da14
github-action-benchmark <github@users.noreply.github.com>
2023-12-06 08:32:18 +0000

    add ICTS_Solver_S2P Benchmark (customSmallerIsBetter) benchmark result for 1a47036796adbe5567b21868bfe3063088a9e368

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701851536941,
+  "lastUpdate": 1701851538618,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -1891,6 +1891,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ICTS_Solver_S2P - Average Expansions Low Level",
             "value": 57715.176,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1a47036796adbe5567b21868bfe3063088a9e368",
+          "message": "More mdd functionality (#76)\n\n* add searchToFirstSolution\r\n\r\n* more tests\r\n\r\n* remove or deprecate some dead code\r\n\r\n* Support constraints in building minimal MDD\r\n\r\n* Improve SingleAgentAStar_Solver to cache results of when there are constraints rejecting staying infinitely on the last move",
+          "timestamp": "2023-12-06T19:02:26+11:00",
+          "tree_id": "4d4db19f38615b0e7e14f1dfcf89c05c6c7bd9cc",
+          "url": "https://github.com/J-morag/MAPF/commit/1a47036796adbe5567b21868bfe3063088a9e368"
+        },
+        "date": 1701851537946,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ICTS_Solver_S2P - Fails",
+            "value": 13,
+            "unit": "Instances"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Cost",
+            "value": 5061,
+            "unit": "SOC"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime",
+            "value": 25570.588,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions High Level",
+            "value": 9465.823,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions Low Level",
+            "value": 38725.35,
             "unit": "Expansions"
           }
         ]
