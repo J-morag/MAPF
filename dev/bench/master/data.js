f521863c0dccc5840687bd059e77d392319e41a5
github-action-benchmark <github@users.noreply.github.com>
2023-12-11 09:31:02 +0000

    add ICTS_Solver_S2P Benchmark (customSmallerIsBetter) benchmark result for c92eb09e28e262e8b0fbef5590d19657f0b6904c

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1702287061043,
+  "lastUpdate": 1702287062169,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -2318,6 +2318,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ICTS_Solver_S2P - Average Expansions High Level",
             "value": 9733.235,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions Low Level",
+            "value": 38768,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "distinct": true,
+          "id": "c92eb09e28e262e8b0fbef5590d19657f0b6904c",
+          "message": "fix releaseMemory in LNS not releasing heuristic",
+          "timestamp": "2023-12-11T20:00:58+11:00",
+          "tree_id": "9945d63222bc218b6cecac5bc9e06b337f4eb6c8",
+          "url": "https://github.com/J-morag/MAPF/commit/c92eb09e28e262e8b0fbef5590d19657f0b6904c"
+        },
+        "date": 1702287061891,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ICTS_Solver_S2P - Fails",
+            "value": 13,
+            "unit": "Instances"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Cost",
+            "value": 5061,
+            "unit": "SOC"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime",
+            "value": 25482.824,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions High Level",
+            "value": 9721.588,
             "unit": "Expansions"
           },
           {
