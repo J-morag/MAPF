5a5783a20e0d90af468c3a3e6e9e63d589c32130
github-action-benchmark <github@users.noreply.github.com>
2024-02-01 06:03:32 +0000

    add ICTS_Solver_S2P Benchmark (customSmallerIsBetter) benchmark result for a481cc5c7df0ad9fcc814e4e7b7712bb49e0f8fc

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706767411410,
+  "lastUpdate": 1706767412319,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -4375,6 +4375,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ICTS_Solver_S2P - Average Expansions Low Level",
             "value": 36328.766,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "distinct": true,
+          "id": "a481cc5c7df0ad9fcc814e4e7b7712bb49e0f8fc",
+          "message": "fix PrioritisedPlanning_Solver name for benchmark",
+          "timestamp": "2024-02-01T16:23:05+11:00",
+          "tree_id": "605f7eec8d41cb5477bd362e088b2365de4eb272",
+          "url": "https://github.com/J-morag/MAPF/commit/a481cc5c7df0ad9fcc814e4e7b7712bb49e0f8fc"
+        },
+        "date": 1706767412085,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ICTS_Solver_S2P - Fails",
+            "value": 13,
+            "unit": "Instances"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Cost",
+            "value": 5290.75,
+            "unit": "SOC"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime",
+            "value": 26777.646,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions High Level",
+            "value": 7461.353,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions Low Level",
+            "value": 36734,
             "unit": "Expansions"
           }
         ]
