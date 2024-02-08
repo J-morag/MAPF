758dd003b21a1ffc870f5351a4252500d6a03e49
github-action-benchmark <github@users.noreply.github.com>
2024-02-08 03:19:39 +0000

    add AStar Benchmark (customSmallerIsBetter) benchmark result for 13114ae3f96fc07abaad92698f5e15b66c3c680b

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1707362378443,
+  "lastUpdate": 1707362379605,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -12412,6 +12412,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "AStar - Average Runtime",
             "value": 1107.4706,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "AStar - Average Expansions Low Level",
+            "value": 396247.8,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "distinct": true,
+          "id": "13114ae3f96fc07abaad92698f5e15b66c3c680b",
+          "message": "add using perfect heuristic in some SIPP tests",
+          "timestamp": "2024-02-08T13:38:04+11:00",
+          "tree_id": "bbd7c94a7b6ca7dc73d44fc292f6f0974572ed6d",
+          "url": "https://github.com/J-morag/MAPF/commit/13114ae3f96fc07abaad92698f5e15b66c3c680b"
+        },
+        "date": 1707362379199,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "AStar - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "AStar - Average Cost",
+            "value": 286.1647,
+            "unit": "SOC"
+          },
+          {
+            "name": "AStar - Average Runtime",
+            "value": 1117.3765,
             "unit": "Milliseconds"
           },
           {
