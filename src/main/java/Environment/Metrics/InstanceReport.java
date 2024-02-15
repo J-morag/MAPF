5888c9d3ec8e8ca4a6805eb8b7639f69ee010ea3
J-morag <38430800+J-morag@users.noreply.github.com>
2024-02-16 09:32:06 +1100

    add runtime to initial solution metric

--- src/main/java/Environment/Metrics/InstanceReport.java
+++ src/main/java/Environment/Metrics/InstanceReport.java
@@ -52,6 +52,7 @@ public class InstanceReport {
         public final static String totalLowLevelTimeMS = "Total Low Level Time (ms)";
         public final static String totalLowLevelCalls = "Total Calls to Low Level";
         public final static String timeoutThresholdMS = "Timeout Threshold";
+        public static final String runtimeToFirstSolution = "Runtime to First Solution";
         public final static String solved = "Solved";
         public final static String valid = "Valid";
         public final static String solutionCost = "Solution Cost";
