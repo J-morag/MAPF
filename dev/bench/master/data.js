75371af47d0bc9517fd7d653e9511a5b400ae12f
github-action-benchmark <github@users.noreply.github.com>
2024-03-13 00:33:09 +0000

    add AStar Benchmark (customSmallerIsBetter) benchmark result for f7e7a672e638c38cf16b19f51f4ea8c02cc9f64b

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710289988487,
+  "lastUpdate": 1710289989563,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -14032,6 +14032,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "AStar - Average Runtime",
             "value": 1081.553,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "AStar - Average Expansions Low Level",
+            "value": 396247.8,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "104091867+NoyGabay@users.noreply.github.com",
+            "name": "NoyGabay",
+            "username": "NoyGabay"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f7e7a672e638c38cf16b19f51f4ea8c02cc9f64b",
+          "message": "Transient CBS (#81)\n\n* add transient parameter in CBS constructor\r\n\r\n* CBSt optimizes sst\r\n\r\n* fix and clean before PR\r\n\r\n* fix and clean before PR\r\n\r\n* Update src/main/java/BasicMAPF/Solvers/CBS/CBS_Solver.java\r\n\r\nCo-authored-by: J-morag <38430800+J-morag@users.noreply.github.com>\r\n\r\n* Update src/main/java/BasicMAPF/Solvers/CBS/CBS_Solver.java\r\n\r\nCo-authored-by: J-morag <38430800+J-morag@users.noreply.github.com>\r\n\r\n* Update src/main/java/Environment/RunManagers/GenericRunManager.java\r\n\r\nCo-authored-by: J-morag <38430800+J-morag@users.noreply.github.com>\r\n\r\n* add test to CBS, still not passing\r\n\r\n* add transient example to CBS tests\r\n\r\n* sst optimization for CBS\r\n\r\n---------\r\n\r\nCo-authored-by: J-morag <38430800+J-morag@users.noreply.github.com>",
+          "timestamp": "2024-03-12T14:46:49+02:00",
+          "tree_id": "1fb562be8a75359718902bb6d0c71194e900d7cf",
+          "url": "https://github.com/J-morag/MAPF/commit/f7e7a672e638c38cf16b19f51f4ea8c02cc9f64b"
+        },
+        "date": 1710289989299,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "AStar - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "AStar - Average Cost",
+            "value": 286.1647,
+            "unit": "SOC"
+          },
+          {
+            "name": "AStar - Average Runtime",
+            "value": 1146.2118,
             "unit": "Milliseconds"
           },
           {
