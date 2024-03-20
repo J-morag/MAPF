e1c235b858fe7f5162f0e939e930d3bce624fbff
github-action-benchmark <github@users.noreply.github.com>
2024-03-20 01:11:19 +0000

    add CBS_SIPP Benchmark (customSmallerIsBetter) benchmark result for 82e96516129ca60eaaf3ef39d30fd158e9e6da99

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1710897078505,
+  "lastUpdate": 1710897079953,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -18209,6 +18209,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "CBS_SIPP - Average Expansions Low Level",
             "value": 8659174,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82e96516129ca60eaaf3ef39d30fd158e9e6da99",
+          "message": "refactor TransientMAPFBehaviour (#86)\n\n* Remove SST from TransientMAPFSettings, use only I_SolutionCostFunction. In PrP, properly initialize subproblem GAndH when using SST cost function\r\n\r\n* fix TransientMAPFExampleMain",
+          "timestamp": "2024-03-20T11:32:57+11:00",
+          "tree_id": "70af99a762404e76353ec7aae86afa3029231397",
+          "url": "https://github.com/J-morag/MAPF/commit/82e96516129ca60eaaf3ef39d30fd158e9e6da99"
+        },
+        "date": 1710897079517,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CBS_SIPP - Fails",
+            "value": 6,
+            "unit": "Instances"
+          },
+          {
+            "name": "CBS_SIPP - Average Cost",
+            "value": 3016,
+            "unit": "SOC"
+          },
+          {
+            "name": "CBS_SIPP - Average Runtime",
+            "value": 29050.766,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS_SIPP - Average Runtime Low Level",
+            "value": 8432.117,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS_SIPP - Average Expansions High Level",
+            "value": 75699.18,
+            "unit": "Expansions"
+          },
+          {
+            "name": "CBS_SIPP - Average Expansions Low Level",
+            "value": 8839240,
             "unit": "Expansions"
           }
         ]
