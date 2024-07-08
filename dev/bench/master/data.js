bd5888fcc9bab0c2407542c418f182b07ff7ba76
github-action-benchmark <github@users.noreply.github.com>
2024-07-08 07:55:34 +0000

    add ALNS Benchmark (customSmallerIsBetter) benchmark result for f9dfde61877e42be27a818623443459822f1cda9

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720425333422,
+  "lastUpdate": 1720425334863,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -9669,6 +9669,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ALNS - Average Expansions Low Level",
             "value": 13046185,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f9dfde61877e42be27a818623443459822f1cda9",
+          "message": "V0.001.0 - LaCAM (#90)\n\n* Lacam\r\n\r\n(cherry picked from commit 5dfb82574e9e06bcca2559f9fa1e9824317c28b8)\r\n\r\n* Updated README.md and docs\r\n\r\n(cherry picked from commit 2f933319e8be511903438c33e0e85a57f150642f)\r\n\r\n---------\r\n\r\nCo-authored-by: NoyGabay <104091867+NoyGabay@users.noreply.github.com>",
+          "timestamp": "2024-07-08T10:13:03+03:00",
+          "tree_id": "e80de395482c10e5858aa0f1acecf8f62fecfa33",
+          "url": "https://github.com/J-morag/MAPF/commit/f9dfde61877e42be27a818623443459822f1cda9"
+        },
+        "date": 1720425334489,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ALNS - Fails",
+            "value": 2,
+            "unit": "Instances"
+          },
+          {
+            "name": "ALNS - Average Cost",
+            "value": 8708.866,
+            "unit": "SOC"
+          },
+          {
+            "name": "ALNS - Average Runtime",
+            "value": 30002.705,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ALNS - Average Runtime Low Level",
+            "value": 27511.941,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ALNS - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ALNS - Average Expansions Low Level",
+            "value": 13936262,
             "unit": "Expansions"
           }
         ]
