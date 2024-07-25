25adbd4d8419e6d5b40cd534e5c6d083fd0e4b9f
J-morag <38430800+J-morag@users.noreply.github.com>
2024-07-25 16:35:33 +0300

    return GenericRunManager to default solvers (prioritized planning and CBS)
    (cherry picked from commit 1ed7cc7d38d885b941c4eae032f1f62a92b1988b)

--- src/main/java/Environment/RunManagers/GenericRunManager.java
+++ src/main/java/Environment/RunManagers/GenericRunManager.java
@@ -50,37 +50,15 @@ public class GenericRunManager extends A_RunManager {
     @Override
     void setSolvers() {
         // TODO modular solvers?
-//        if (solversOverride != null){
-//            super.solvers = solversOverride;
-//            return;
-//        }
-//        super.solvers.add(new PrioritisedPlanning_Solver(null, null, null,
-//                new RestartsStrategy(RestartsStrategy.RestartsKind.none, 0, RestartsStrategy.RestartsKind.randomRestarts),
-//                null, null, null));
+        if (solversOverride != null){
+            super.solvers = solversOverride;
+            return;
+        }
+        super.solvers.add(new PrioritisedPlanning_Solver(null, null, null,
+                new RestartsStrategy(RestartsStrategy.RestartsKind.none, 0, RestartsStrategy.RestartsKind.randomRestarts),
+                null, null, null));
 
         super.solvers.add(new CBS_Solver());
-
-        PrioritisedPlanning_Solver pp = new PrioritisedPlanning_Solver(new SingleAgentAStar_Solver(), null, null,
-                new RestartsStrategy(RestartsStrategy.RestartsKind.none, 0, RestartsStrategy.RestartsKind.none),
-                null, null, null);
-        pp.name = "PP-no-restarts";
-        super.solvers.add(pp);
-
-        PrioritisedPlanning_Solver ppRandomAStar = new PrioritisedPlanning_Solver(new SingleAgentAStar_Solver(), null, null,
-                new RestartsStrategy(RestartsStrategy.RestartsKind.AStarRestarts, 10000000, RestartsStrategy.RestartsKind.none),
-                null, null, null);
-        ppRandomAStar.name = "PP-rand-AStar";
-        super.solvers.add(ppRandomAStar);
-
-        PriorityConstrainedSearch pcs = new PCSBuilder().setUseSimpleMDDCache(true).setMDDCacheDepthDeltaMax(1)
-                .setUsePartialGeneration(true).setPCSHeuristic(new PCSHeuristicDefault()).createPCS();
-        pcs.name = "PCS";
-        super.solvers.add(pcs);
-
-        PriorityConstrainedSearch PCS_SIPPH = new PCSBuilder().setUseSimpleMDDCache(true).setMDDCacheDepthDeltaMax(1)
-                .setUsePartialGeneration(true).setPCSHeuristic(new PCSHeuristicSIPP()).createPCS();
-        PCS_SIPPH.name = "PCS_SIPPH";
-        super.solvers.add(PCS_SIPPH);
     }
 
     public void overrideSolvers(@NotNull List<I_Solver> solvers){
