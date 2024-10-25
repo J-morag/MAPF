e73c587348cbd5563c88abc8204551ded59ea3c0
github-action-benchmark <github@users.noreply.github.com>
2024-10-25 04:18:48 +0000

    add SIPP Benchmark (customSmallerIsBetter) benchmark result for aa3bc8038cdc38941bfdd9ee2a68ae483cd122c3

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729829926683,
+  "lastUpdate": 1729829928435,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -21868,6 +21868,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "SIPP - Average Runtime",
             "value": 341.05884,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "SIPP - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "SIPP - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "SIPP - Average Expansions Low Level",
+            "value": 23324.754,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "aa3bc8038cdc38941bfdd9ee2a68ae483cd122c3",
+          "message": "v0.02.0 (#92)\n\n* add PCS tie breaking for smaller MDDs\r\n* Canonize testing benchmark and comparative diverse test functions\r\n* add Config static class\r\n* add debug and info to command line args\r\n* remove I_SolutionCostFunction.addCommonCostsToReport from CBS\r\n* Add support for new instance format with arbitrary graphs\r\n* performance: instant location lookup in GraphBasedGridMap\r\n* return GenericRunManager to default solvers (prioritized planning and CBS)\r\n* change PriorityConstrainedSearch to use PCSCompTieBreakSmallerMDDs by default\r\n* Fix comparative test averages reporting\r\n* add RunParametersBuilder copy function\r\n* consolidate more reporting and metrics in A_Solver\r\n* workaround for LaCAMStar_Solver nodes counting\r\n* fix typos in InstanceReport.StandardFields \r\n* switch CBS_Solver to using the builder pattern",
+          "timestamp": "2024-10-25T14:35:26+11:00",
+          "tree_id": "b5b37e5baaf04f28c252f20a69d2e671539b6674",
+          "url": "https://github.com/J-morag/MAPF/commit/aa3bc8038cdc38941bfdd9ee2a68ae483cd122c3"
+        },
+        "date": 1729829927739,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SIPP - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "SIPP - Average Cost",
+            "value": 286.1647,
+            "unit": "SOC"
+          },
+          {
+            "name": "SIPP - Average Runtime",
+            "value": 380.7294,
             "unit": "Milliseconds"
           },
           {
