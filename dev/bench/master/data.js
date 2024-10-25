a745e81e774bb55ca9f63cea06e22f09e85ec469
github-action-benchmark <github@users.noreply.github.com>
2024-10-25 07:47:04 +0000

    add LaCAM Benchmark (customSmallerIsBetter) benchmark result for 900fe56557dc22da59e34cb82ff035d09a0834c6

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729842423131,
+  "lastUpdate": 1729842424503,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -25723,6 +25723,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "LaCAM - Average Expansions Low Level",
             "value": 192124.12,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "J-morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "900fe56557dc22da59e34cb82ff035d09a0834c6",
+          "message": "v0.06.0 (#96)\n\n* Add low level expansion/generation rate, and add all rates to A_RunManager output (\"log\" files)\r\n\r\n* add generation and expansion rates for low-level\r\n\r\n* simplify (and optimize?) TieBreakingForLessConflictsAndHigherG\r\n\r\n* lacam constraints support\r\n\r\n* Add regenerated nodes metric. Low level only.\r\n\r\n* simplify Move.equals",
+          "timestamp": "2024-10-25T18:05:03+11:00",
+          "tree_id": "53b2eddd6ea90c050d365e76f97eb20a65817f1b",
+          "url": "https://github.com/J-morag/MAPF/commit/900fe56557dc22da59e34cb82ff035d09a0834c6"
+        },
+        "date": 1729842424092,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "LaCAM - Fails",
+            "value": 2,
+            "unit": "Instances"
+          },
+          {
+            "name": "LaCAM - Average Cost",
+            "value": 50977.066,
+            "unit": "SOC"
+          },
+          {
+            "name": "LaCAM - Average Runtime",
+            "value": 4505.1763,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Expansions High Level",
+            "value": 1372.5883,
+            "unit": "Expansions"
+          },
+          {
+            "name": "LaCAM - Average Expansions Low Level",
+            "value": 273527,
             "unit": "Expansions"
           }
         ]
