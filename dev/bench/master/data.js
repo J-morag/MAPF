76ac178bff2ad11b424123852c8f6e896610ae57
github-action-benchmark <github@users.noreply.github.com>
2025-05-19 16:21:16 +0000

    add Priority Constrained Search Benchmark (customSmallerIsBetter) benchmark result for c21042cff21a499975c904785f5e3af5283c9be1

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747671675133,
+  "lastUpdate": 1747671676254,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -26747,6 +26747,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "Priority Constrained Search - Average Expansions Low Level",
             "value": 1831779,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c21042cff21a499975c904785f5e3af5283c9be1",
+          "message": "v0.09.0 (#101)\n\n* Handle safe intervals generation in constraint set \n\n* add comparator to A* constructor\n\n* SIPP - improve clarity and performance\n\n* replace clearing with nulling for slightly better performance\n\n* Bucket queue open list \n\n* ArrayMap only check keys if `DEBUG >= 1`\n\n* PCS use BucketingOpenList\n\n* Canonical solvers in prog args \n\n* Lexical PCS cost function \n\n* Use CanonicalSolversFactory for basic solver versions in tests",
+          "timestamp": "2025-05-19T18:38:17+03:00",
+          "tree_id": "3a1483dfb02690ad65f671611e4048caf124fb4f",
+          "url": "https://github.com/J-morag/MAPF/commit/c21042cff21a499975c904785f5e3af5283c9be1"
+        },
+        "date": 1747671675863,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Priority Constrained Search - Fails",
+            "value": 5,
+            "unit": "Instances"
+          },
+          {
+            "name": "Priority Constrained Search - Average Cost",
+            "value": 1857.6666,
+            "unit": "SOC"
+          },
+          {
+            "name": "Priority Constrained Search - Average Runtime",
+            "value": 11465.294,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "Priority Constrained Search - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "Priority Constrained Search - Average Expansions High Level",
+            "value": 12146.294,
+            "unit": "Expansions"
+          },
+          {
+            "name": "Priority Constrained Search - Average Expansions Low Level",
+            "value": 1850558.9,
             "unit": "Expansions"
           }
         ]
