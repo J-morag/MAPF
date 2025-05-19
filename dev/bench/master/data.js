2a115888830640d7e3059bc346e66d4acac0da22
github-action-benchmark <github@users.noreply.github.com>
2025-05-19 15:23:20 +0000

    add CBS Benchmark (customSmallerIsBetter) benchmark result for 413ea0221e99da6bdf750be8e1213f3b594efbc0

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747665368191,
+  "lastUpdate": 1747668200808,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -3671,6 +3671,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "CBS - Average Expansions Low Level",
             "value": 5504511.5,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "413ea0221e99da6bdf750be8e1213f3b594efbc0",
+          "message": "v.0.08.0 (#100)\n\n* rename RemovableConflictAvoidance to RemovableConflictCounter for clarity\n\n* 1. Multiple bug fixes RemovableConflictAvoidanceTableWithContestedGoals leading to double counting of vertex conflicts.\n2. Tests for RemovableConflictAvoidanceTableWithContestedGoals\n3. SingleAgentAStar_Solver partial implementation of full tie-breaking for fewer conflicts in TMAPF context. Missing considering \"IsALastMove\" type states as separate and counting their conflicts.\n\n* 1. add TMAPF AStar Conflicts after target tie breaking support\n2. AStar: refactor generate function.\n3. AStar: Require VisitedTargetAStarGoalCondition if ServiceTimeGAndH and vice versa.\n4. CBS, LNS, PrP: Require SumServiceTimes if isTransientMAPF and vice versa.\n\n* add PrP with SIPP to performance benchmark, and add printing average expansions on solved instances only",
+          "timestamp": "2025-05-19T17:40:06+03:00",
+          "tree_id": "f390a39a876fe64429a4a4593fb96f1bafc7324d",
+          "url": "https://github.com/J-morag/MAPF/commit/413ea0221e99da6bdf750be8e1213f3b594efbc0"
+        },
+        "date": 1747668198984,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CBS - Fails",
+            "value": 8,
+            "unit": "Instances"
+          },
+          {
+            "name": "CBS - Average Cost",
+            "value": 3141.5557,
+            "unit": "SOC"
+          },
+          {
+            "name": "CBS - Average Runtime",
+            "value": 17067,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Runtime Low Level",
+            "value": 6634.294,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Expansions High Level",
+            "value": 34553.35,
+            "unit": "Expansions"
+          },
+          {
+            "name": "CBS - Average Expansions Low Level",
+            "value": 5825390.5,
             "unit": "Expansions"
           }
         ]
