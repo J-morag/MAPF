b42600d22dc452a73f87cf911a724b78ea03b738
github-action-benchmark <github@users.noreply.github.com>
2025-05-19 15:23:27 +0000

    add PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts Benchmark (customSmallerIsBetter) benchmark result for 413ea0221e99da6bdf750be8e1213f3b594efbc0

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747668206040,
+  "lastUpdate": 1747668207828,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -26751,6 +26751,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions Low Level",
             "value": 6058265.5,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "413ea0221e99da6bdf750be8e1213f3b594efbc0",
+          "message": "v.0.08.0 (#100)\n\n* rename RemovableConflictAvoidance to RemovableConflictCounter for clarity\n\n* 1. Multiple bug fixes RemovableConflictAvoidanceTableWithContestedGoals leading to double counting of vertex conflicts.\n2. Tests for RemovableConflictAvoidanceTableWithContestedGoals\n3. SingleAgentAStar_Solver partial implementation of full tie-breaking for fewer conflicts in TMAPF context. Missing considering \"IsALastMove\" type states as separate and counting their conflicts.\n\n* 1. add TMAPF AStar Conflicts after target tie breaking support\n2. AStar: refactor generate function.\n3. AStar: Require VisitedTargetAStarGoalCondition if ServiceTimeGAndH and vice versa.\n4. CBS, LNS, PrP: Require SumServiceTimes if isTransientMAPF and vice versa.\n\n* add PrP with SIPP to performance benchmark, and add printing average expansions on solved instances only",
+          "timestamp": "2025-05-19T17:40:06+03:00",
+          "tree_id": "f390a39a876fe64429a4a4593fb96f1bafc7324d",
+          "url": "https://github.com/J-morag/MAPF/commit/413ea0221e99da6bdf750be8e1213f3b594efbc0"
+        },
+        "date": 1747668207066,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Fails",
+            "value": 3,
+            "unit": "Instances"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Cost",
+            "value": 10543,
+            "unit": "SOC"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Runtime",
+            "value": 12279,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Runtime Low Level",
+            "value": 10906.294,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions Low Level",
+            "value": 5508423,
             "unit": "Expansions"
           }
         ]
