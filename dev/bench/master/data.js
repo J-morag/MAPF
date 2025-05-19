cdcbf94331bc4f83cf41e9299e7e648f3a371215
github-action-benchmark <github@users.noreply.github.com>
2025-05-19 14:35:52 +0000

    add CBS Benchmark (customSmallerIsBetter) benchmark result for 54c89b1f45a03f68d752ab95bb1764a5a77789fe

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729842426984,
+  "lastUpdate": 1747665352326,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -3617,6 +3617,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "CBS - Average Expansions Low Level",
             "value": 2736691,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "54c89b1f45a03f68d752ab95bb1764a5a77789fe",
+          "message": "v0.07.0 (#99)\n\n* Transient seperating vertices\n\n* Add results printing function to test utils, with running example usi… \n\n* add SV support for PrPt and CBSt \n\n* add support for resolving conflicts locally in CBSt \n\n* improve GridSolutionVisualizer colors and fix missing last time step \n\n* Better colors in GridSolutionVisualizer:\n\n* improve GridVisualizer GUI and allow timestep step +- wrapping\n\n* improve GridVisualizer X/Y dimensions clarity",
+          "timestamp": "2025-05-19T16:54:24+03:00",
+          "tree_id": "009e114d127543b1697267ba69e4403aecf1cacf",
+          "url": "https://github.com/J-morag/MAPF/commit/54c89b1f45a03f68d752ab95bb1764a5a77789fe"
+        },
+        "date": 1747665350685,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CBS - Fails",
+            "value": 8,
+            "unit": "Instances"
+          },
+          {
+            "name": "CBS - Average Cost",
+            "value": 3141.5557,
+            "unit": "SOC"
+          },
+          {
+            "name": "CBS - Average Runtime",
+            "value": 17678.824,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Runtime Low Level",
+            "value": 6582.2354,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Expansions High Level",
+            "value": 32414.295,
+            "unit": "Expansions"
+          },
+          {
+            "name": "CBS - Average Expansions Low Level",
+            "value": 5504511.5,
             "unit": "Expansions"
           }
         ]
