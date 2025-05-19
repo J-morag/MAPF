9f1ee4b617e07f09073d54f4c2be118cd40fdc55
github-action-benchmark <github@users.noreply.github.com>
2025-05-19 18:08:39 +0000

    add SIPP Benchmark (customSmallerIsBetter) benchmark result for 584f590bb27ad0519be222950a549307bee6c33f

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747678117650,
+  "lastUpdate": 1747678119310,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -25285,6 +25285,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "SIPP - Average Expansions Low Level",
             "value": 30930.447,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "584f590bb27ad0519be222950a549307bee6c33f",
+          "message": "v.0.10.0 (#102)\n\n* testing shows SIPP expands significantly more nodes with the bucketing open list, so I disabled it in SIPP until I find out why this is happening\n\n* Mapf lns2\n\n* disable using BucketingOpenList in A*",
+          "timestamp": "2025-05-19T20:26:15+03:00",
+          "tree_id": "5c3ef5f36ca83e81a03300fb136bc2b1be916924",
+          "url": "https://github.com/J-morag/MAPF/commit/584f590bb27ad0519be222950a549307bee6c33f"
+        },
+        "date": 1747678118578,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SIPP - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "SIPP - Average Cost",
+            "value": 286.1647,
+            "unit": "SOC"
+          },
+          {
+            "name": "SIPP - Average Runtime",
+            "value": 289.0706,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "SIPP - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "SIPP - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "SIPP - Average Expansions Low Level",
+            "value": 23324.754,
             "unit": "Expansions"
           }
         ]
