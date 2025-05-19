9094ba1dd765e112c04ff0b9cdebd91bb7177746
github-action-benchmark <github@users.noreply.github.com>
2025-05-19 15:23:24 +0000

    add ICTS_Solver_S2P Benchmark (customSmallerIsBetter) benchmark result for 413ea0221e99da6bdf750be8e1213f3b594efbc0

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747668202539,
+  "lastUpdate": 1747668204314,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -7399,6 +7399,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ICTS_Solver_S2P - Average Expansions Low Level",
             "value": 41209.234,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "413ea0221e99da6bdf750be8e1213f3b594efbc0",
+          "message": "v.0.08.0 (#100)\n\n* rename RemovableConflictAvoidance to RemovableConflictCounter for clarity\n\n* 1. Multiple bug fixes RemovableConflictAvoidanceTableWithContestedGoals leading to double counting of vertex conflicts.\n2. Tests for RemovableConflictAvoidanceTableWithContestedGoals\n3. SingleAgentAStar_Solver partial implementation of full tie-breaking for fewer conflicts in TMAPF context. Missing considering \"IsALastMove\" type states as separate and counting their conflicts.\n\n* 1. add TMAPF AStar Conflicts after target tie breaking support\n2. AStar: refactor generate function.\n3. AStar: Require VisitedTargetAStarGoalCondition if ServiceTimeGAndH and vice versa.\n4. CBS, LNS, PrP: Require SumServiceTimes if isTransientMAPF and vice versa.\n\n* add PrP with SIPP to performance benchmark, and add printing average expansions on solved instances only",
+          "timestamp": "2025-05-19T17:40:06+03:00",
+          "tree_id": "f390a39a876fe64429a4a4593fb96f1bafc7324d",
+          "url": "https://github.com/J-morag/MAPF/commit/413ea0221e99da6bdf750be8e1213f3b594efbc0"
+        },
+        "date": 1747668203549,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ICTS_Solver_S2P - Fails",
+            "value": 12,
+            "unit": "Instances"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Cost",
+            "value": 4351.4,
+            "unit": "SOC"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime",
+            "value": 22041.354,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions High Level",
+            "value": 10202.647,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions Low Level",
+            "value": 41210.883,
             "unit": "Expansions"
           }
         ]
