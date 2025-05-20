c129cab114582bc2860b500869a3c3e289882eb0
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 08:00:21 +0000

    add CBS Benchmark (customSmallerIsBetter) benchmark result for 69342d2acfbc0a54edea9a884a8e34220593af5e

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747720708926,
+  "lastUpdate": 1747728021696,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -3887,6 +3887,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "CBS - Average Expansions Low Level",
             "value": 6853448,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "69342d2acfbc0a54edea9a884a8e34220593af5e",
+          "message": "v.0.12.0 (#104)\n\n* Add TMAPF GAndH validations to LaCAM_Solver and PIBT_Solver :\n1. Add TMAPF GAndH validations to LaCAM_Solver and PIBT_Solver .\n2. Initialize solution cost functions and GAndH automatically according to TMAPF/MAPF by default.\n\n* fix bug in insert nodes to OPEN in SIPP\n\n* add transient algorithms to CanonicalSolversFactory, and other algorithms such as PIE and LNS1 with SIPP\n\n* add getTransientMAPFSettings to I_Solver\n\n* bugfix missing swapping conflict in RemovableConflictAvoidanceTableWithContestedGoals\n\n* fix PIBT CanonicalSolversFactory initialization\n\n* PIBT init from CanononicalSovlerFactory in PerformanceBenchmarkTest\n\n* CBS_SIPP in CanonicalSolversFactory",
+          "timestamp": "2025-05-20T09:20:08+03:00",
+          "tree_id": "f10e1afc06203a27a671c27faa4f1644bfc4cb3d",
+          "url": "https://github.com/J-morag/MAPF/commit/69342d2acfbc0a54edea9a884a8e34220593af5e"
+        },
+        "date": 1747728020342,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CBS - Fails",
+            "value": 19,
+            "unit": "Instances"
+          },
+          {
+            "name": "CBS - Average Cost",
+            "value": 4376.5713,
+            "unit": "SOC"
+          },
+          {
+            "name": "CBS - Average Runtime",
+            "value": 19069.06,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Runtime Low Level",
+            "value": 10373.939,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "CBS - Average Expansions High Level",
+            "value": 19058.06,
+            "unit": "Expansions"
+          },
+          {
+            "name": "CBS - Average Expansions Low Level",
+            "value": 6783448,
             "unit": "Expansions"
           }
         ]
