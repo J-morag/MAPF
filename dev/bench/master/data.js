a6e7ba6d8cc4f44ad55dbedc2da5f2f05a409448
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 05:58:13 +0000

    add PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts Benchmark (customSmallerIsBetter) benchmark result for 6a36001f2b0443f23777fb6b21014037fa9ce809

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747720691236,
+  "lastUpdate": 1747720693460,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -28371,6 +28371,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions Low Level",
             "value": 5689552,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6a36001f2b0443f23777fb6b21014037fa9ce809",
+          "message": "v.0.11.0 (#103)\n\n* improve A_ConflictAvoidanceTable numConflicts performance \n\n* Avoid unnecessary state generations for goal check in SingleAgentAStar_Solver. Only relevant where we have a conflict avoidance table.\n\n* reduce the overhead of SIPP intermediate wait state generation \n\n* Add more instances to comparative test and performance benchmark\n\n* Edge conflicts in I_ConflictAvoidanceTable",
+          "timestamp": "2025-05-20T07:19:49+03:00",
+          "tree_id": "8d00148ac8a1e0c3621ad88d3264dd6151d9bc59",
+          "url": "https://github.com/J-morag/MAPF/commit/6a36001f2b0443f23777fb6b21014037fa9ce809"
+        },
+        "date": 1747720692728,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Fails",
+            "value": 9,
+            "unit": "Instances"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Cost",
+            "value": 12504.125,
+            "unit": "SOC"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Runtime",
+            "value": 17635.666,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Runtime Low Level",
+            "value": 17371.576,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions Low Level",
+            "value": 7443846,
             "unit": "Expansions"
           }
         ]
