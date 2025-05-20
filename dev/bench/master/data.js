5d4ddbbd904825d1105901e26d1d2a47cacf616b
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 08:00:33 +0000

    add LaCAM Benchmark (customSmallerIsBetter) benchmark result for 69342d2acfbc0a54edea9a884a8e34220593af5e

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747728031813,
+  "lastUpdate": 1747728032988,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -28639,6 +28639,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "LaCAM - Average Expansions Low Level",
             "value": 77370.18,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "69342d2acfbc0a54edea9a884a8e34220593af5e",
+          "message": "v.0.12.0 (#104)\n\n* Add TMAPF GAndH validations to LaCAM_Solver and PIBT_Solver :\n1. Add TMAPF GAndH validations to LaCAM_Solver and PIBT_Solver .\n2. Initialize solution cost functions and GAndH automatically according to TMAPF/MAPF by default.\n\n* fix bug in insert nodes to OPEN in SIPP\n\n* add transient algorithms to CanonicalSolversFactory, and other algorithms such as PIE and LNS1 with SIPP\n\n* add getTransientMAPFSettings to I_Solver\n\n* bugfix missing swapping conflict in RemovableConflictAvoidanceTableWithContestedGoals\n\n* fix PIBT CanonicalSolversFactory initialization\n\n* PIBT init from CanononicalSovlerFactory in PerformanceBenchmarkTest\n\n* CBS_SIPP in CanonicalSolversFactory",
+          "timestamp": "2025-05-20T09:20:08+03:00",
+          "tree_id": "f10e1afc06203a27a671c27faa4f1644bfc4cb3d",
+          "url": "https://github.com/J-morag/MAPF/commit/69342d2acfbc0a54edea9a884a8e34220593af5e"
+        },
+        "date": 1747728032584,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "LaCAM - Fails",
+            "value": 7,
+            "unit": "Instances"
+          },
+          {
+            "name": "LaCAM - Average Cost",
+            "value": 113818.54,
+            "unit": "SOC"
+          },
+          {
+            "name": "LaCAM - Average Runtime",
+            "value": 8101.879,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Expansions High Level",
+            "value": 2444.697,
+            "unit": "Expansions"
+          },
+          {
+            "name": "LaCAM - Average Expansions Low Level",
+            "value": 147186,
             "unit": "Expansions"
           }
         ]
