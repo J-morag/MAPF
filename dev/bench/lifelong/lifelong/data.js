141429e4dadb44f68a383e5c8600e773da2e3a68
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 10:12:13 +0000

    add PIBT_h10 Benchmark (customBiggerIsBetter) benchmark result for 594d771a465b923036b57b648e0052b313645d93

--- dev/bench/lifelong/lifelong/data.js
+++ dev/bench/lifelong/lifelong/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747735931970,
+  "lastUpdate": 1747735933420,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "stationaryAgentsPrPDeepPartialAvoidFPRHCR_w10_h03Lookahead5 Benchmark": [
@@ -3287,6 +3287,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "PIBT_h10 - Average Runtime Offline Solver (Reciprocal)",
             "value": 0.000027343822,
+            "unit": "1 / Milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "594d771a465b923036b57b648e0052b313645d93",
+          "message": "Update README.md (#106)\n\nfix links",
+          "timestamp": "2025-05-20T13:00:16+03:00",
+          "tree_id": "7250ace8891bf7b42f00cb0324a8481b58d2e401",
+          "url": "https://github.com/J-morag/MAPF/commit/594d771a465b923036b57b648e0052b313645d93"
+        },
+        "date": 1747735932826,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "PIBT_h10 - Average Throughput",
+            "value": 1636.3334,
+            "unit": "Throughput @ T=300"
+          },
+          {
+            "name": "PIBT_h10 - Average Runtime (Reciprocal)",
+            "value": 0.000024120021,
+            "unit": "1 / Milliseconds"
+          },
+          {
+            "name": "PIBT_h10 - Average Runtime Offline Solver (Reciprocal)",
+            "value": 0.00002752925,
             "unit": "1 / Milliseconds"
           }
         ]
