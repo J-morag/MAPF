3e24b19e80f7daca67488a69c9008e7c991cb363
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 11:37:55 +0000

    add SIPP Benchmark (customSmallerIsBetter) benchmark result for 594d771a465b923036b57b648e0052b313645d93

--- dev/bench/lifelong/offline/data.js
+++ dev/bench/lifelong/offline/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747741074365,
+  "lastUpdate": 1747741075397,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -8748,6 +8748,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "SIPP - Average Runtime",
             "value": 365.71515,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "SIPP - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "SIPP - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "SIPP - Average Expansions Low Level",
+            "value": 33255.965,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "594d771a465b923036b57b648e0052b313645d93",
+          "message": "Update README.md (#106)\n\nfix links",
+          "timestamp": "2025-05-20T13:00:16+03:00",
+          "tree_id": "7250ace8891bf7b42f00cb0324a8481b58d2e401",
+          "url": "https://github.com/J-morag/MAPF/commit/594d771a465b923036b57b648e0052b313645d93"
+        },
+        "date": 1747741075109,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "SIPP - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "SIPP - Average Cost",
+            "value": 359.34546,
+            "unit": "SOC"
+          },
+          {
+            "name": "SIPP - Average Runtime",
+            "value": 344.43637,
             "unit": "Milliseconds"
           },
           {
