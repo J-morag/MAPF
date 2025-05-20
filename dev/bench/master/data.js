764e4f79ae7e0f175724bddfcb952db38caa8457
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 05:58:08 +0000

    add ICTS_Solver_S2P Benchmark (customSmallerIsBetter) benchmark result for 6a36001f2b0443f23777fb6b21014037fa9ce809

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747720686247,
+  "lastUpdate": 1747720688703,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -7723,6 +7723,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ICTS_Solver_S2P - Average Expansions Low Level",
             "value": 41210.883,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6a36001f2b0443f23777fb6b21014037fa9ce809",
+          "message": "v.0.11.0 (#103)\n\n* improve A_ConflictAvoidanceTable numConflicts performance \n\n* Avoid unnecessary state generations for goal check in SingleAgentAStar_Solver. Only relevant where we have a conflict avoidance table.\n\n* reduce the overhead of SIPP intermediate wait state generation \n\n* Add more instances to comparative test and performance benchmark\n\n* Edge conflicts in I_ConflictAvoidanceTable",
+          "timestamp": "2025-05-20T07:19:49+03:00",
+          "tree_id": "8d00148ac8a1e0c3621ad88d3264dd6151d9bc59",
+          "url": "https://github.com/J-morag/MAPF/commit/6a36001f2b0443f23777fb6b21014037fa9ce809"
+        },
+        "date": 1747720687924,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ICTS_Solver_S2P - Fails",
+            "value": 24,
+            "unit": "Instances"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Cost",
+            "value": 4421.1113,
+            "unit": "SOC"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime",
+            "value": 23170.666,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions High Level",
+            "value": 8746.516,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions Low Level",
+            "value": 106346.39,
             "unit": "Expansions"
           }
         ]
