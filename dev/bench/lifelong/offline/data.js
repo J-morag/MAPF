8e792a58a7a5beee9b2b04aed31ad8f54d650e15
github-action-benchmark <github@users.noreply.github.com>
2025-05-20 11:37:48 +0000

    add PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts Benchmark (customSmallerIsBetter) benchmark result for 594d771a465b923036b57b648e0052b313645d93

--- dev/bench/lifelong/offline/data.js
+++ dev/bench/lifelong/offline/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747741067085,
+  "lastUpdate": 1747741068198,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -9093,6 +9093,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions Low Level",
             "value": 7135418.5,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "594d771a465b923036b57b648e0052b313645d93",
+          "message": "Update README.md (#106)\n\nfix links",
+          "timestamp": "2025-05-20T13:00:16+03:00",
+          "tree_id": "7250ace8891bf7b42f00cb0324a8481b58d2e401",
+          "url": "https://github.com/J-morag/MAPF/commit/594d771a465b923036b57b648e0052b313645d93"
+        },
+        "date": 1747741067789,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Fails",
+            "value": 9,
+            "unit": "Instances"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Cost",
+            "value": 12504.125,
+            "unit": "SOC"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Runtime",
+            "value": 17286.666,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Runtime Low Level",
+            "value": 17017.242,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "PrP (AStar) + min. 10 attempts, initial reorderings: randomRestarts - Average Expansions Low Level",
+            "value": 7574826,
             "unit": "Expansions"
           }
         ]
