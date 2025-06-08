657af1f5618ced974e1e571c9af97dc51a0217af
github-action-benchmark <github@users.noreply.github.com>
2025-06-08 16:03:35 +0000

    add LaCAM Benchmark (customSmallerIsBetter) benchmark result for 226e4b9434c9ee295bda17de5c6faa83f9caa53a

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1749398613661,
+  "lastUpdate": 1749398615061,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -29935,6 +29935,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "LaCAM - Average Expansions Low Level",
             "value": 239076.25,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "226e4b9434c9ee295bda17de5c6faa83f9caa53a",
+          "message": "Update README.md",
+          "timestamp": "2025-06-08T17:16:49+03:00",
+          "tree_id": "e0dd3a531df3df8ca4fbf94544c860ce1dd98919",
+          "url": "https://github.com/J-morag/MAPF/commit/226e4b9434c9ee295bda17de5c6faa83f9caa53a"
+        },
+        "date": 1749398614526,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "LaCAM - Fails",
+            "value": 7,
+            "unit": "Instances"
+          },
+          {
+            "name": "LaCAM - Average Cost",
+            "value": 113709.38,
+            "unit": "SOC"
+          },
+          {
+            "name": "LaCAM - Average Runtime",
+            "value": 7969.9697,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Expansions High Level",
+            "value": 2230.303,
+            "unit": "Expansions"
+          },
+          {
+            "name": "LaCAM - Average Expansions Low Level",
+            "value": 260420.78,
             "unit": "Expansions"
           }
         ]
