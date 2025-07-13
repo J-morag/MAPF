9caab9b190ce82abb7e75205e5a3a9e1fa5b94f0
github-action-benchmark <github@users.noreply.github.com>
2025-07-13 13:06:29 +0000

    add ICTS_Solver_S2P Benchmark (customSmallerIsBetter) benchmark result for db0cf5dd465dcda71ff694c64d24fb59087d0084

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1752411987695,
+  "lastUpdate": 1752411989400,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -8371,6 +8371,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "ICTS_Solver_S2P - Average Expansions Low Level",
             "value": 106264.79,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "db0cf5dd465dcda71ff694c64d24fb59087d0084",
+          "message": "Bump org.apache.commons:commons-lang3 from 3.13.0 to 3.18.0 (#108)\n\nBumps org.apache.commons:commons-lang3 from 3.13.0 to 3.18.0.\n\n---\nupdated-dependencies:\n- dependency-name: org.apache.commons:commons-lang3\n  dependency-version: 3.18.0\n  dependency-type: direct:production\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-07-13T14:21:38+03:00",
+          "tree_id": "73a3d1a03f2756b78109cf03c2e25c38773ffdfd",
+          "url": "https://github.com/J-morag/MAPF/commit/db0cf5dd465dcda71ff694c64d24fb59087d0084"
+        },
+        "date": 1752411988690,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ICTS_Solver_S2P - Fails",
+            "value": 24,
+            "unit": "Instances"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Cost",
+            "value": 4421.1113,
+            "unit": "SOC"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime",
+            "value": 23122.516,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions High Level",
+            "value": 8993.151,
+            "unit": "Expansions"
+          },
+          {
+            "name": "ICTS_Solver_S2P - Average Expansions Low Level",
+            "value": 106433.39,
             "unit": "Expansions"
           }
         ]
