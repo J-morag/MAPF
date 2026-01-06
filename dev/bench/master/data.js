aa7418d3a3c1f2a5737f6bf54557ca906e569713
github-action-benchmark <github@users.noreply.github.com>
2026-01-06 16:22:33 +0000

    add AStar Benchmark (customSmallerIsBetter) benchmark result for f845132facc3978738b3abf187cb80029379f025

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767716551298,
+  "lastUpdate": 1767716553516,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -20782,6 +20782,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "AStar - Average Runtime",
             "value": 1416.8606,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "AStar - Average Expansions High Level",
+            "value": 0,
+            "unit": "Expansions"
+          },
+          {
+            "name": "AStar - Average Expansions Low Level",
+            "value": 538202.3,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f845132facc3978738b3abf187cb80029379f025",
+          "message": "v0.14.0 Multi-agent A* (#109)\n\n* ConflictAvoidanceTable horizon support\n\n* remove unnecessary copying of lastConstraintTime in ConstraintSet copy constructor, that could lead to errors\n\n* add support for a limited time horizon in ConflictAvoidanceTable s\n\n* add PathDelays and FreespaceCosts metrics (vectors) \n\n* Multi Agent A*\n\n* MAA* Lexical cost function\n\n* MultiAgent A* with Operator Decomposition",
+          "timestamp": "2026-01-06T16:34:47+02:00",
+          "tree_id": "b8ab45720c11f5e9cc0d23a959aaa64f70be163e",
+          "url": "https://github.com/J-morag/MAPF/commit/f845132facc3978738b3abf187cb80029379f025"
+        },
+        "date": 1767716553144,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "AStar - Fails",
+            "value": 0,
+            "unit": "Instances"
+          },
+          {
+            "name": "AStar - Average Cost",
+            "value": 359.34546,
+            "unit": "SOC"
+          },
+          {
+            "name": "AStar - Average Runtime",
+            "value": 1502.6727,
             "unit": "Milliseconds"
           },
           {
