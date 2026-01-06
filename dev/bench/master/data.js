eed6e7a11625a9966beb970279e1417a6355e4d7
github-action-benchmark <github@users.noreply.github.com>
2026-01-06 16:22:31 +0000

    add LaCAM Benchmark (customSmallerIsBetter) benchmark result for f845132facc3978738b3abf187cb80029379f025

--- dev/bench/master/data.js
+++ dev/bench/master/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767716549747,
+  "lastUpdate": 1767716551298,
   "repoUrl": "https://github.com/J-morag/MAPF",
   "entries": {
     "CBS Benchmark": [
@@ -31231,6 +31231,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "LaCAM - Average Expansions Low Level",
             "value": 71402.516,
+            "unit": "Expansions"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38430800+J-morag@users.noreply.github.com",
+            "name": "Jonathan Morag",
+            "username": "J-morag"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f845132facc3978738b3abf187cb80029379f025",
+          "message": "v0.14.0 Multi-agent A* (#109)\n\n* ConflictAvoidanceTable horizon support\n\n* remove unnecessary copying of lastConstraintTime in ConstraintSet copy constructor, that could lead to errors\n\n* add support for a limited time horizon in ConflictAvoidanceTable s\n\n* add PathDelays and FreespaceCosts metrics (vectors) \n\n* Multi Agent A*\n\n* MAA* Lexical cost function\n\n* MultiAgent A* with Operator Decomposition",
+          "timestamp": "2026-01-06T16:34:47+02:00",
+          "tree_id": "b8ab45720c11f5e9cc0d23a959aaa64f70be163e",
+          "url": "https://github.com/J-morag/MAPF/commit/f845132facc3978738b3abf187cb80029379f025"
+        },
+        "date": 1767716550917,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "LaCAM - Fails",
+            "value": 6,
+            "unit": "Instances"
+          },
+          {
+            "name": "LaCAM - Average Cost",
+            "value": 110722.516,
+            "unit": "SOC"
+          },
+          {
+            "name": "LaCAM - Average Runtime",
+            "value": 7045.212,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Runtime Low Level",
+            "value": 0,
+            "unit": "Milliseconds"
+          },
+          {
+            "name": "LaCAM - Average Expansions High Level",
+            "value": 1624.091,
+            "unit": "Expansions"
+          },
+          {
+            "name": "LaCAM - Average Expansions Low Level",
+            "value": 250565.52,
             "unit": "Expansions"
           }
         ]
